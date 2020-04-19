24af5cc5539a6d778276cd9e8f18712096960b8a
shun <3939s.w@gmail.com>
2020-04-19 17:30:03 +0900

    add build directory for production

---
+++ build/precache-manifest.a9c8d3b2798edc32440261984623a9db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cabcd8b5779af4189f449c949c54b9dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb0817a8fa2901e67939",
+    "url": "/static/js/2.8419f07b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8419f07b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f48b5bfdcc6d83445713",
+    "url": "/static/js/main.f5a6d91e.chunk.js"
+  },
+  {
+    "revision": "3d09fc7fbfc0228634fe",
+    "url": "/static/js/runtime-main.7266208d.js"
+  }
+]);
